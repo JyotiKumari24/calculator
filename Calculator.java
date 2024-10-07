11893429e083210cf314f50b22a7e9d67da20268
JyotiKumari24 <jk992017@gmail.com>
2024-10-07 15:41:29 +0530

    Calculation

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+abstract class Calculator
+{
+	public void display()
+	{
+		System.out.println("To Perform All the Calculation in Calculator");
+	}
+	public void add(int a, intb);
+	public void sub(int a, intb);
+	public void mult(int a,int b);
+	public void div(int a, int b);
+}
